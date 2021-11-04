1ab4a5d85c06e9ee4746e63b215c59fe3a653c8f
jeff <10823453+jeffmc@users.noreply.github.com>
2021-11-04 12:20:12 -0700

    Added game logic, stepping with a button

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function GameOfLife() {
     this.canvas = document.getElementById("game");
     (this.canvasWidth = 600), (this.canvasHeight = 600); // TODO: Fix this.
     this.gfx = this.canvas.getContext("2d");
-    (this.fieldWidth = 25), (this.fieldHeight = 25); // TODO: Fix canvas aspect ratio for square cells.
+    (this.fieldWidth = 50), (this.fieldHeight = 50); // TODO: Fix canvas aspect ratio for square cells.
 
     this.infoParagraph = document.getElementById("info");
     this.infoParagraph.innerHTML = `<b>${this.fieldWidth}x${
@@ -24,40 +24,19 @@ function GameOfLife() {
 
     // clear background
     this.gfx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-    this.generateField(); // generate blank field
+    this.field = this.generateField(); // generate blank field
     this.drawField();
   };
 
   this.generateField = function () {
     // width, height, and number of mines
-    this.field = [];
+    let newField = [];
     let totalSize = this.fieldWidth * this.fieldHeight;
     let x, y; // x and y coordinates for the below for loop.
     for (let i = 0; i < totalSize; i++) {
-      this.field[i] = false;
+      newField[i] = false;
     }
-  };
-
-  this.stepCell = function (x, y) {
-    let idx = this.getFieldIDX(x, y);
-    let xp = x < this.fieldWidth - 1,
-      xn = x > 0; // x +/- available
-    let yp = y < this.fieldHeight - 1,
-      yn = y > 0; // y +/- available
-    let neighbors = 0;
-    if (xp && yp) this.field[this.getFieldIDX(x + 1, y + 1)]++;
-    if (xn && yp) this.field[this.getFieldIDX(x - 1, y + 1)]++;
-    if (xp && yn) this.field[this.getFieldIDX(x + 1, y - 1)]++;
-    if (xn && yn) this.field[this.getFieldIDX(x - 1, y - 1)]++;
-    if (xp) this.field[this.getFieldIDX(x + 1, y)]++;
-    if (xn) this.field[this.getFieldIDX(x - 1, y)]++;
-    if (yp) this.field[this.getFieldIDX(x, y + 1)]++;
-    if (yn) this.field[this.getFieldIDX(x, y - 1)]++;
-  };
-
-  this.toggleCell = function (x, y) {
-    let idx = this.getFieldIDX(x, y);
-    field[idx] = !field[idx];
+    return newField;
   };
 
   this.getFieldIDX = function (x, y) {
@@ -72,8 +51,56 @@ function GameOfLife() {
     return Math.floor(idx / this.fieldWidth);
   };
 
+  this.stepCell = function (x, y, cf, nf) {
+    // If alive
+    //   0 or 1 neighbors - dies of loneliness
+    //   2 or 3 neighbors - survives to next round
+    //   4 or more neighbors - dies of overcrowding
+    // If dead
+    //   3 neighbors - becomes populated
+
+    // cf is currentField, nf is nextField
+    let idx = this.getFieldIDX(x, y);
+    let xp = x < this.fieldWidth - 1,
+      xn = x > 0; // x +/- available
+    let yp = y < this.fieldHeight - 1,
+      yn = y > 0; // y +/- available
+    let neighbors = 0;
+    if (xp && yp)
+      neighbors += this.field[this.getFieldIDX(x + 1, y + 1)] ? 1 : 0;
+    if (xn && yp)
+      neighbors += this.field[this.getFieldIDX(x - 1, y + 1)] ? 1 : 0;
+    if (xp && yn)
+      neighbors += this.field[this.getFieldIDX(x + 1, y - 1)] ? 1 : 0;
+    if (xn && yn)
+      neighbors += this.field[this.getFieldIDX(x - 1, y - 1)] ? 1 : 0;
+    if (xp) neighbors += this.field[this.getFieldIDX(x + 1, y)] ? 1 : 0;
+    if (xn) neighbors += this.field[this.getFieldIDX(x - 1, y)] ? 1 : 0;
+    if (yp) neighbors += this.field[this.getFieldIDX(x, y + 1)] ? 1 : 0;
+    if (yn) neighbors += this.field[this.getFieldIDX(x, y - 1)] ? 1 : 0;
+    let nextState = false;
+    if (cf[idx]) {
+      if (neighbors >= 2 && neighbors <= 3) {
+        nextState = true;
+      }
+    } else {
+      if (neighbors == 3) nextState = true;
+    }
+    nf[idx] = nextState;
+  };
+
+  this.stepField = function () {
+    let nextField = [];
+    for (let y = 0; y < this.fieldHeight; y++) {
+      for (let x = 0; x < this.fieldWidth; x++) {
+        this.stepCell(x, y, this.field, nextField);
+      }
+    }
+    this.field = nextField;
+  };
+
   this.drawCell = function (x, y, state) {
-    this.gfx.fillStyle = state ? "#000055" : "#000000";
+    this.gfx.fillStyle = state ? "#0000aa" : "#000000";
     this.gfx.fillRect(
       x * this.cellWidth,
       y * this.cellHeight,
@@ -81,24 +108,46 @@ function GameOfLife() {
       this.cellHeight
     );
   };
+
   this.drawField = function () {
     for (let y = 0; y < this.fieldHeight; y++) {
       for (let x = 0; x < this.fieldWidth; x++) {
-        this.drawCell(x,y,this.field[y * this.fieldWidth + x]);
+        this.drawCell(x, y, this.field[y * this.fieldWidth + x]);
       }
     }
   };
-  this.step = function () {
-    let nextField = [];
+  this.update = function () {
+    this.stepField();
+    this.drawField();
   };
+  this.userToggleCell = function (x, y) {
+    // Should only be called in response to user input.
+    let idx = this.getFieldIDX(x, y);
+    this.field[idx] = !this.field[idx];
+    this.drawCell(x,y,this.field[idx]);
+  };
+  this.clicked = function(e) {
+    let cellX = Math.floor(e.offsetX / this.cellWidth);
+    let cellY = Math.floor(e.offsetY / this.cellHeight);
+    this.userToggleCell(cellX,cellY);
+  }
 }
+
+
 let container = {};
 container.gameOfLife = new GameOfLife();
+function tickit() {
+  this.gameOfLife.update();
+}
+function canvasClick(e) {
+  this.gameOfLife.clicked(e);
+}
+tickit = tickit.bind(container);
+canvasClick = canvasClick.bind(container);
 function start() {
   container.gameOfLife.setup();
   // setInterval(tickit.bind(container), 50);
 }
-function tickit() {
-  this.gameOfLife.step();
-}
 window.addEventListener("load", start);
+document.querySelector("#step").addEventListener("click", tickit);
+document.querySelector("#game").addEventListener("click", canvasClick);
